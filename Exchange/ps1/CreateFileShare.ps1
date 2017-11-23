3e670eb7f586de3380bf11e18dbdb800182b122d
John Eldridge <john.eldridge@realec.com>
2017-11-22 19:33:52 -0600

    a

--- Exchange/ps1/CreateFileShare.ps1
+++ Exchange/ps1/CreateFileShare.ps1
@@ -3,18 +3,18 @@ Param (
   [String]$SAKey,
   [String]$SAName
 )
- $VerbosePreference = "Continue"
- Write-Verbose "Starting creation of file share $SAName"
+ #$VerbosePreference = "Continue"
+ #Write-Verbose "Starting creation of file share $SAName"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Write-Verbose "Nuget Installed"
+#Write-Verbose "Nuget Installed"
 
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module Azure -Confirm:$False
 Import-Module Azure
-Write-Verbose "PS Gallery COnfigured"
+#Write-Verbose "PS Gallery COnfigured"
 
 $storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SourceSAKey
-Write-Verbose "Storage Context Set"
+#Write-Verbose "Storage Context Set"
 $storageContext |  New-AzureStorageShare -Name 'ExchangeFiles'
-Write-Verbose "Fileshare created"
+#Write-Verbose "Fileshare created"
